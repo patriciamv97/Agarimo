063c2a1ec82285660b01fb551e47ac70fa00cc7e
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2022-02-24 19:34:57 +0100

    Implemento interfaz en el fragment_registro_profesional.xml para poder darle funcionalidad al boton del fragment

--- app/src/main/java/com/example/agarimo/ActivitySignUp.kt
+++ app/src/main/java/com/example/agarimo/ActivitySignUp.kt
@@ -7,13 +7,14 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import android.view.ViewParent
+import android.widget.Button
+import android.widget.Toast
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.example.agarimo.databinding.ActivitySignUpBinding
 
-class ActivitySignUp : AppCompatActivity() {
-
+class ActivitySignUp : AppCompatActivity(), RegistroProfesional.CallBack {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -28,9 +29,19 @@ class ActivitySignUp : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainer, RegistroProfesional())
                 .commit()
+
         }
     }
 
+    override fun onClickButton() {
+        Toast.makeText(this,"El profesional ha sido registrado", Toast.LENGTH_SHORT).show()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, ProfesionalRegistrado())
+            .addToBackStack(null)
+            .commit()
+
+    }
 
 
 }
--- app/src/main/java/com/example/agarimo/RegistroProfesional.kt
+++ app/src/main/java/com/example/agarimo/RegistroProfesional.kt
@@ -1,10 +1,13 @@
 package com.example.agarimo
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import java.lang.ClassCastException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +20,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RegistroProfesional : Fragment() {
+    lateinit var myCallBack: RegistroProfesional.CallBack
+
+    interface CallBack{
+        fun onClickButton()
+    }
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,6 +44,24 @@ class RegistroProfesional : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_registro_profesional, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        R.id.btnRegistrarP
+        val btnRegistrarP = view.findViewById<Button>(R.id.btnRegistrarP)
+        btnRegistrarP.setOnClickListener{
+            myCallBack.onClickButton()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            myCallBack = context as CallBack
+        }catch (e : ClassCastException){
+            throw ClassCastException (context?.toString()+ "must implement ToolbarListener")
+        }
     }
 
     companion object {
